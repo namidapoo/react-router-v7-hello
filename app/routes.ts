b0e48af2e200627306a99703aaa0f7d723e71781
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-02-23 11:12:50 +0900

    docs: ルーティングのコメント追加

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,5 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
+// これと同義（多分`index`用のヘルパー関数が用意されてるだけ）
+// export default [route("/", "routes/home.tsx")] satisfies RouteConfig;
 export default [index("routes/home.tsx")] satisfies RouteConfig;
