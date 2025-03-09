d95417a9f1b3ba3ba5c4955f9ae3dd522fd470b2
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-03-09 15:28:44 +0900

    feat: リンクの追加

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -1,7 +1,16 @@
 import type { FC } from "react";
+import { Link } from "react-router";
+import { Button } from "~/components/ui/button";
 
 const Main: FC = () => {
-	return <div>Main</div>;
+	return (
+		<div>
+			Main
+			<Button asChild>
+				<Link to="projects/new">New</Link>
+			</Button>
+		</div>
+	);
 };
 
 export default Main;
